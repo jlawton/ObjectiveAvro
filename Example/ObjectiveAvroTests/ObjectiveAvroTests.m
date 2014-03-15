5993a47accc00ec47c55e38768ae3c7ee74ac9bb
Marcelo Fabri <marcelofabrimf@gmail.com>
2014-03-14 23:52:44 -0300

    Tests

--- Example/ObjectiveAvroTests/ObjectiveAvroTests.m
+++ Example/ObjectiveAvroTests/ObjectiveAvroTests.m
@@ -191,5 +191,208 @@
     expect(data).to.beNil();
 }
 
+#pragma mark - Type tests 
+
+- (void)testStringType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"StringTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"string_value\",\"type\":\"string\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+
+    NSArray *strings = @[@"bla", @"test", @"foo", @"bar"];
+    
+    for (NSString *str in strings) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"string_value": str} forSchemaNamed:@"StringTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *strFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"StringTest" error:&error][@"string_value"];
+        
+        expect(error).to.beNil();
+        expect(strFromAvro).to.equal(str);
+    }
+}
+
+- (void)testIntType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"IntTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"int_value\",\"type\":\"int\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    NSArray *numbers = @[@2, @303, @1098, @500000, @-200, @-100001, @0];
+    
+    for (NSNumber *number in numbers) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"int_value": number} forSchemaNamed:@"IntTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *numberFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"IntTest" error:&error][@"int_value"];
+        
+        expect(error).to.beNil();
+        expect(numberFromAvro).to.equal(number);
+    }
+}
+
+- (void)testLongType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"LongTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"long_value\",\"type\":\"long\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    NSArray *numbers = @[@2, @303, @1098, @500000,  @-200, @-100001, @0, @((long) pow(2, 30)), @((long) pow(-2, 30))];
+    
+    for (NSNumber *number in numbers) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"long_value": number} forSchemaNamed:@"LongTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *numberFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"LongTest" error:&error][@"long_value"];
+        
+        expect(error).to.beNil();
+        expect(numberFromAvro).to.equal(number);
+    }
+}
+
+- (void)testFloatType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"FloatTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"float_value\",\"type\":\"float\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    NSArray *numbers = @[@2, @303, @1098, @500000, @-200, @-100001, @0, @1.43f, @100.98420f, @0.001f, @-9.7431f];
+    
+    for (NSNumber *number in numbers) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"float_value": number} forSchemaNamed:@"FloatTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *numberFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"FloatTest" error:&error][@"float_value"];
+        
+        expect(error).to.beNil();
+        expect(numberFromAvro).to.equal(number);
+    }
+}
+
+- (void)testDoubleType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"DoubleTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"double_value\",\"type\":\"double\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    NSArray *numbers = @[@2, @303, @1098, @500000, @-200, @-100001, @0, @1.43, @100.98420, @0.001, @-9.7431, @(DBL_MAX), @((double) pow(2.4, 20)), @(M_PI)];
+    
+    for (NSNumber *number in numbers) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"double_value": number} forSchemaNamed:@"DoubleTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *numberFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"DoubleTest" error:&error][@"double_value"];
+        
+        expect(error).to.beNil();
+        expect(numberFromAvro).to.equal(number);
+    }
+}
+
+- (void)testBooleanType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"BooleanTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"boolean_value\",\"type\":\"boolean\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    NSArray *numbers = @[@NO, @YES];
+    
+    for (NSNumber *number in numbers) {
+        NSError *error;
+        NSData *data = [avro dataFromJSONObject:@{@"boolean_value": number} forSchemaNamed:@"BooleanTest" error:&error];
+        expect(error).to.beNil();
+        expect(data).toNot.beNil();
+        
+        NSString *numberFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"BooleanTest" error:&error][@"boolean_value"];
+        
+        expect(error).to.beNil();
+        expect(numberFromAvro).to.equal(number);
+    }
+}
+
+- (void)testNullType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"NullTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"null_value\",\"type\":\"null\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    
+    NSError *error;
+    NSData *data = [avro dataFromJSONObject:@{@"null_value": [NSNull null]} forSchemaNamed:@"NullTest" error:&error];
+    expect(error).to.beNil();
+    expect(data).toNot.beNil();
+    
+    id nullFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"NullTest" error:&error][@"null_value"];
+    
+    expect(error).to.beNil();
+    expect(nullFromAvro).to.equal([NSNull null]);
+}
+
+- (void)testArrayType {
+    NSString *schema = @"{\"type\":\"array\",\"name\":\"ArrayTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"items\": {\"type\": \"int\"}}";
+
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    
+    NSError *error;
+    NSArray *array = @[@1, @5, @-2, @0, @10021, @500000];
+    NSData *data = [avro dataFromJSONObject:array forSchemaNamed:@"ArrayTest" error:&error];
+    expect(error).to.beNil();
+    expect(data).toNot.beNil();
+    
+    NSArray *arrayFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"ArrayTest" error:&error];
+    
+    expect(error).to.beNil();
+    expect(arrayFromAvro).to.equal(array);
+}
+
+- (void)testMapType {
+    NSString *schema = @"{\"type\":\"map\",\"name\":\"MapTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"values\": \"int\"}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    
+    NSError *error;
+    NSDictionary *map = @{@"one": @1, @"zero": @0, @"two": @2, @"-one": @-1};
+    NSData *data = [avro dataFromJSONObject:map forSchemaNamed:@"MapTest" error:&error];
+    expect(error).to.beNil();
+    expect(data).toNot.beNil();
+    
+    NSDictionary *mapFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"MapTest" error:&error];
+    
+    expect(error).to.beNil();
+    expect(mapFromAvro).to.equal(map);
+}
+
+- (void)testBytesType {
+    NSString *schema = @"{\"type\":\"record\",\"name\":\"BytesTest\",\"namespace\":\"com.movile.objectiveavro.unittest.v1\",\"fields\":[{\"name\":\"bytes_value\",\"type\":\"bytes\"}]}";
+    
+    OAVAvroSerialization *avro = [[OAVAvroSerialization alloc] init];
+    [avro registerSchema:schema error:NULL];
+    
+    
+    NSError *error;
+    NSString *bytes = @"\"\\u00de\\u00ad\\u00be\\u00ef\"";
+    
+    NSData *data = [avro dataFromJSONObject:@{@"bytes_value": bytes} forSchemaNamed:@"BytesTest" error:&error];
+    expect(error).to.beNil();
+    expect(data).toNot.beNil();
+    
+    id bytesFromAvro = [avro JSONObjectFromData:data forSchemaNamed:@"BytesTest" error:&error][@"bytes_value"];
+    
+    expect(error).to.beNil();
+    expect(bytesFromAvro).to.equal(bytes);
+}
 
 @end
